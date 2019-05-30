c5e827375a0a5635c31d08f31dc05fefaf49b2e6
Oscar Persson <oscar.persson@hiq.se>
2019-05-30 17:30:30 +0200

    Improved flame

--- flame/js/script.js
+++ flame/js/script.js
@@ -6,12 +6,20 @@ let mouseX = 0;
 let mouseY = 0;
 let prevX = 0;
 let prevY = 0;
+let spawnAccu = 0;
 let onCanvas = false;
 
-let SPAWN_RATE = 2;
+let SPAWN_RATE = 150;
+let MAX_SPEED = 12;
 let MAX_WIDTH = 1;
-let system;
+let FPS = 60;
+let START_RADIUS = 10;
+let GROW_RADIUS = 60;
+let LIFE_LENGTH = 1;
+let GRAVITY = 700;
+let MAX_START_VEL = 150;
 
+let system;
 let loopTimer;
 let lastUpdate;
 
@@ -28,7 +36,7 @@ window.onload = function() {
     canvas.addEventListener('mouseleave', leave);
     windowResize();
 
-    loopTimer = setInterval(draw, 0);
+    loopTimer = setInterval(draw, 1000 / FPS);
     lastUpdate = Date.now();
 };
 
@@ -43,8 +51,6 @@ function move(e) {
 }
 function enter(e) {
     onCanvas = true;
-    prevX = mouseX = e.x;
-    prevY = mouseY = e.y;
 }
 function leave(e) {
     onCanvas = false;
@@ -56,21 +62,27 @@ function rgb(r, g, b) {
 
 let draw = function() {
     var now = Date.now();
-    var dt = now - lastUpdate;
+    var dt = (now - lastUpdate) * 0.001;
     lastUpdate = now;
 
-    if (onCanvas) {
-        let dX = mouseX - prevX;
-        let dY = mouseY - prevY;
-        dX /= SPAWN_RATE;
-        dY /= SPAWN_RATE;
-        for (let i = 0; i < SPAWN_RATE; ++i) {
-            system.addParticle(prevX + dX * i, prevY + dY * i);
-        }
-
-        prevX = mouseX;
-        prevY = mouseY;
+    if (!onCanvas) {
+        mouseX = canvas.width * 0.5;
+        mouseY = canvas.height * 0.8;
     }
+    let dX = mouseX - prevX;
+    let dY = mouseY - prevY;
+    let len = Math.sqrt(dX * dX + dY * dY);
+    let lenInv = 0;
+    if (len > 0) lenInv = 1 / len;
+    dX *= lenInv;
+    dY *= lenInv;
+    spawnAccu += SPAWN_RATE * dt;
+    while (spawnAccu >= 1) {
+        system.addParticle(prevX, prevY);
+        spawnAccu -= 1;
+    }
+    prevX += dX * MAX_SPEED * dt * len;
+    prevY += dY * MAX_SPEED * dt * len;
     gc.fillStyle = rgb(48, 48, 48);
     gc.fillRect(0, 0, canvas.width, canvas.height);
     system.draw(dt);
@@ -78,12 +90,6 @@ let draw = function() {
 
 class ParticleSystem {
     constructor() {
-        this.START_RADIUS = 10;
-        this.GROW_RADIUS = 0.3;
-        this.LIFE_LENGTH = 1;
-        this.DT = 0.016;
-        this.GRAVITY = 0.05;
-
         this.particles = [];
     }
 
@@ -95,10 +101,10 @@ class ParticleSystem {
         this.particles.push({
             x: x,
             y: y,
-            vx: Math.random() * 2 - 1,
-            vy: Math.random() * 2 - 1,
-            r: this.START_RADIUS,
-            t: this.LIFE_LENGTH
+            vx: (Math.random() * 2 - 1) * MAX_START_VEL,
+            vy: (Math.random() * 2 - 1) * MAX_START_VEL,
+            r: START_RADIUS,
+            t: LIFE_LENGTH
         });
     }
 
@@ -113,9 +119,9 @@ class ParticleSystem {
     updateParticle(p, dt) {
         p.x += p.vx * dt;
         p.y += p.vy * dt;
-        p.vy -= this.GRAVITY;
-        p.r += this.GROW_RADIUS * dt;
-        p.t -= this.dt;
+        p.vy -= GRAVITY * FPS * dt * dt;
+        p.r += GROW_RADIUS * dt;
+        p.t -= dt;
 
         gc.fillStyle = rgb(48 + p.t * 207, 48 + p.t * p.t * 152, 48);
         gc.beginPath();
